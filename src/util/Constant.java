193fe32508658f252be1ed36b839e730d5816178
Phynic <dream1994001@163.com>
2015-08-09 23:30:15 +0800

    Create Constant.java

---
+++ src/util/Constant.java
@@ -0,0 +1,16 @@
+package util;
+
+public class Constant {
+	//游戏分辨率
+    public static final int GAME_WIDTH = 1366;
+    public static final int GAME_HEIGHT = 768;
+    //初始坦克位置
+    public static final int MYTANK1_X = 400+3;
+    public static final int MYTANK1_Y = 687+28;
+    public static final int MYTANK2_X = 700+3;
+    public static final int MYTANK2_Y = 687+28;
+    //障碍物单位
+    public static final int OBSTRUCTION_WIDTH = 50;
+    public static final int OBSTRUCTION_HEIGHT = 50;
+    
+}
